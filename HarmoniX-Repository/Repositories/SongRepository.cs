1dae2d522648e32648cb69bbff8e90719a628c68
Hyudeque <quanghuy01062004@gmail.com>
2024-09-09 17:03:26 +0700

    Update SongRepository.cs

--- HarmoniX-Repository/Repositories/SongRepository.cs
+++ HarmoniX-Repository/Repositories/SongRepository.cs
@@ -1,22 +1,17 @@
 ﻿using HarmoniX_Repository.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HarmoniX_Repository.Repositories
 {
     public class SongRepository
     {
-         private HarmonixDbContext _context;
+        private HarmonixDbContext _context;
 
         public async Task CreateSongAsync(Song song)
         {
             _context = new();
-                _context.Songs.Add(song);
-                await _context.SaveChangesAsync();
+            _context.Songs.Add(song);
+            await _context.SaveChangesAsync();
         }
 
         public async Task UpdateSongAsync(Song song)
